1775413a4ae165c67ed755d53e9c35b7b9391328
Dima <zyryanovdima97@gmail.com>
2023-11-21 21:42:22 +0300

    1

--- script.js
+++ script.js
@@ -30,3 +30,53 @@ if (savedTheme) {
     // По умолчанию устанавливаем светлую тему
     setTheme('light-theme');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggleBtn = document.getElementById('themeToggleBtn');
+    const intro = document.querySelector('.intro');
+
+    themeToggleBtn.addEventListener('click', function () {
+        intro.classList.toggle('dark-theme');
+        
+        // Проверяем, какая тема активна и меняем фон .intro соответственно
+        if (intro.classList.contains('dark-theme')) {
+            intro.style.backgroundImage = "url('img/img2.jpg')";
+        } else {
+            intro.style.backgroundImage = "url('img/img1.jpg')";
+        }
+    });
+});
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector(".navbar");
+    var intro = document.querySelector(".intro");
+
+    function updateNavbar() {
+        var scrollPosition = window.scrollY || window.pageYOffset;
+
+        if (scrollPosition === 0) {
+            navbar.style.backgroundColor = "rgba(51, 51, 51, 0)";
+        } else {
+            navbar.style.backgroundColor = "rgba(51, 51, 51, 0.5)"; // Измените цвет по необходимости
+        }
+    }
+
+    // Обновляем состояние шапки при загрузке и прокрутке
+    updateNavbar();
+    window.addEventListener("scroll", updateNavbar);
+
+    const themeToggleBtn = document.getElementById('themeToggleBtn');
+
+    themeToggleBtn.addEventListener('click', function () {
+        intro.classList.toggle('dark-theme');
+
+        // Проверяем, какая тема активна и меняем фон .intro соответственно
+        if (intro.classList.contains('dark-theme')) {
+            intro.style.backgroundImage = "url('img/img2.jpg')";
+        } else {
+            intro.style.backgroundImage = "url('img/img1.jpg')";
+        }
+    });
+});
